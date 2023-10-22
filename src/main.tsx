0c680029d5f0d1643e81bae8ba26e2316683f5fd
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-10-22 22:46:04 +0300

    feat: add base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './styles/global.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById('root') as HTMLElement;
+
+if (root) ReactDOM.createRoot(root).render(<App />);
