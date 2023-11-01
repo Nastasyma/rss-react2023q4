3d832d85918c683e6261ee1c2af93f05c3b14f84
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-11-01 15:47:33 +0300

    refactor: efit router logic

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,20 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import { RoutesName } from '../utils/types';
-import Root from '../components/Root';
 import ErrorBoundary from '../components/Error/ErrorBoundary';
 import HomePage from '../pages/HomePage/HomePage';
+import CardPage from '../pages/CardPage/CardPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path={RoutesName.home} element={<Root />}>
-      <Route
-        index
-        element={
-          <ErrorBoundary>
-            <HomePage />
-          </ErrorBoundary>
-        }
-      />
+    <Route
+      path={RoutesName.home}
+      element={
+        <ErrorBoundary>
+          <HomePage />
+        </ErrorBoundary>
+      }
+    >
+      <Route path="" element={<CardPage />} />
     </Route>
   )
 );
