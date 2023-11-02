75ad76221fb8acb7c694b41b24abfa3ea9f062a9
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-11-02 13:13:24 +0300

    refactor: edit detailed card styles, add close button

--- src/components/DetailedCard/DetailedCard.tsx
+++ src/components/DetailedCard/DetailedCard.tsx
@@ -1,35 +1,35 @@
+import { useSearchParams } from 'react-router-dom';
 import { ICard } from '../../utils/types';
 import styles from './DetailedCard.module.scss';
-import KnifeIcon from '../../assets/images/knife-and-spoon.svg?react';
-import TreeIcon from '../../assets/images/tree.svg?react';
-import SeasonsIcon from '../../assets/images/seansons.svg?react';
 
 interface DetailedCardProps {
   card: ICard;
 }
 
 function DetailedCard({ card }: DetailedCardProps): JSX.Element {
-  const { title, edibility, image, habitat, season, description } = card;
+  const { title, image, description } = card;
+  const [, setSearchParams] = useSearchParams();
 
   return (
     <div className={styles.card}>
-      <div className={styles.cardImg}>
-        <img src={image} alt={`${title} image`} />
+      <div className={styles.cardInfo}>
+        <button
+          type="button"
+          onClick={() => {
+            setSearchParams((searchParams) => {
+              searchParams.delete('mushroom');
+              return searchParams;
+            });
+          }}
+        >
+          Close
+        </button>
+        <div className={styles.cardImg}>
+          <img src={image} alt={`${title} image`} />
+        </div>
+        <h2>{title}</h2>
+        <div className={styles.cardDescription}>{description}</div>
       </div>
-      <h2>{title}</h2>
-      <div className={styles.cardIcon}>
-        <KnifeIcon />
-        <span>{edibility}</span>
-      </div>
-      <div className={styles.cardIcon}>
-        <TreeIcon />
-        <span>{habitat}</span>
-      </div>
-      <div className={styles.cardIcon}>
-        <SeasonsIcon />
-        <span>{season}</span>
-      </div>
-      <div className={styles.cardDescription}>{description}</div>
     </div>
   );
 }
