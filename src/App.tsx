2fedd970f6b94597be4977616dd6149ba4e67c5c
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-11-03 15:30:54 +0300

    refactor: edit routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import ErrorBoundary from './components/Error/ErrorBoundary';
 
 function App(): JSX.Element {
   return (
     <>
-      <ErrorBoundary>
-        <RouterProvider router={router} />
-      </ErrorBoundary>
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/global.scss';
+import ErrorBoundary from './components/Error/ErrorBoundary.tsx';
 
 const root = document.getElementById('root') as HTMLElement;
 
-if (root) ReactDOM.createRoot(root).render(<App />);
+if (root)
+  ReactDOM.createRoot(root).render(
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  );
