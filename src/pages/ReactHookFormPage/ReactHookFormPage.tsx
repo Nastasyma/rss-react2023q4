f60fc75112ed6d048c4072af7d8d19f41f32055d
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-11-29 16:00:28 +0300

    refactor: edit form styles

--- src/pages/ReactHookFormPage/ReactHookFormPage.tsx
+++ src/pages/ReactHookFormPage/ReactHookFormPage.tsx
@@ -4,6 +4,7 @@ import { Input } from '../../components/Input/Input';
 import styles from './ReactHookFormPage.module.scss';
 import { countries } from '../../utils/countries';
 import { useState } from 'react';
+import { PasswordInput } from '../../components/PasswordInput/PasswordInput';
 
 interface ReactHookFormFields {
   name: string;
@@ -17,7 +18,7 @@ interface ReactHookFormFields {
 
 const initReactHookForm = {
   name: '',
-  age: 0,
+  age: undefined,
   email: '',
   password: '',
   confirmPassword: '',
@@ -44,157 +45,156 @@ function ReactHookFormPage() {
 
   return (
     <FormWrapper title="React Hook Form" buttonText="Submit" disableBtn={false}>
-      <form onSubmit={handleSubmit(onSubmit)}></form>
-      <Controller
-        name="name"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }): JSX.Element => {
-          return (
-            <Input
-              id="1"
-              type="text"
-              placeholder="Name"
-              label="Name:"
-              {...field}
-              error={errors.name}
-            />
-          );
-        }}
-      />
-      <Controller
-        name="age"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }): JSX.Element => {
-          return (
-            <Input
-              id="2"
-              type="number"
-              placeholder="Age"
-              label="Age:"
-              {...field}
-              error={errors.age}
-            />
-          );
-        }}
-      />
-      <Controller
-        name="email"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }): JSX.Element => {
-          return (
-            <Input
-              id="3"
-              type="text"
-              placeholder="Email"
-              label="Email:"
-              {...field}
-              error={errors.email}
-            />
-          );
-        }}
-      />
-      <Controller
-        name="password"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }): JSX.Element => {
-          return (
-            <Input
-              id="4"
-              type="text"
-              placeholder="Password"
-              label="Password:"
-              {...field}
-              error={errors.password}
-            />
-          );
-        }}
-      />
-      <Controller
-        name="confirmPassword"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }): JSX.Element => {
-          return (
-            <Input
-              id="5"
-              type="text"
-              placeholder="Confirm Password"
-              label="Confirm Password:"
-              {...field}
-              error={errors.confirmPassword}
-            />
-          );
-        }}
-      />
-      <Controller
-        name="gender"
-        control={control}
-        rules={{ required: true }}
-        render={({ field }): JSX.Element => {
-          return (
-            <>
-              <label htmlFor="6" className={styles.label}>
-                Gender:
-                <select id="6" placeholder="Gender" {...field} className={styles.input}>
-                  <option value="" disabled selected>
-                    Select an option
-                  </option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                </select>
-              </label>
-            </>
-          );
-        }}
-      />
-      <Controller
-        name="countries"
-        control={control}
-        rules={{ required: true }}
-        render={(): JSX.Element => {
-          return (
-            <>
-              <label htmlFor="7" className={styles.label}>
-                Countries:
-                <input
-                  id="7"
-                  type="text"
-                  placeholder="Country"
-                  list="countries"
-                  className={styles.input}
-                />
-                <datalist id="countries">
-                  {countries.map((country, index) => (
-                    <option key={index} value={country}>
-                      {country}
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <Controller
+          name="name"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }): JSX.Element => {
+            return (
+              <Input
+                id="1"
+                type="text"
+                placeholder="Name"
+                label="Name:"
+                {...field}
+                error={errors.name}
+              />
+            );
+          }}
+        />
+        <Controller
+          name="age"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }): JSX.Element => {
+            return (
+              <Input
+                id="2"
+                type="number"
+                placeholder="Age"
+                label="Age:"
+                {...field}
+                error={errors.age}
+              />
+            );
+          }}
+        />
+        <Controller
+          name="email"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }): JSX.Element => {
+            return (
+              <Input
+                id="3"
+                type="text"
+                placeholder="Email"
+                label="Email:"
+                {...field}
+                error={errors.email}
+              />
+            );
+          }}
+        />
+        <Controller
+          name="password"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }): JSX.Element => {
+            return (
+              <PasswordInput
+                id="4"
+                placeholder="Password"
+                label="Password:"
+                {...field}
+                error={errors.password}
+              />
+            );
+          }}
+        />
+        <Controller
+          name="confirmPassword"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }): JSX.Element => {
+            return (
+              <PasswordInput
+                id="5"
+                placeholder="Confirm Password"
+                label="Confirm Password:"
+                {...field}
+                error={errors.confirmPassword}
+              />
+            );
+          }}
+        />
+        <Controller
+          name="gender"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }): JSX.Element => {
+            return (
+              <>
+                <label htmlFor="6" className={styles.label}>
+                  Gender:
+                  <select id="6" placeholder="Gender" {...field} className={styles.input}>
+                    <option value="" disabled selected>
+                      Select gender
                     </option>
-                  ))}
-                </datalist>
-              </label>
-            </>
-          );
-        }}
-      />
-      <Controller
-        name="countries"
-        control={control}
-        rules={{ required: true }}
-        render={(): JSX.Element => {
-          return (
-            <>
-              <label htmlFor="8">
-                Accept T&C:
-                <input id="8" type="checkbox" />
-                {errorMessage && <span className={styles.errorMessage}>{errorMessage}</span>}
-              </label>
-            </>
-          );
-        }}
-      />
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </label>
+              </>
+            );
+          }}
+        />
+        <Controller
+          name="countries"
+          control={control}
+          rules={{ required: true }}
+          render={(): JSX.Element => {
+            return (
+              <>
+                <label htmlFor="7" className={styles.label}>
+                  Countries:
+                  <input
+                    id="7"
+                    type="text"
+                    placeholder="Country"
+                    list="countries"
+                    className={styles.input}
+                  />
+                  <datalist id="countries">
+                    {countries.map((country, index) => (
+                      <option key={index} value={country}>
+                        {country}
+                      </option>
+                    ))}
+                  </datalist>
+                </label>
+              </>
+            );
+          }}
+        />
+        <Controller
+          name="countries"
+          control={control}
+          rules={{ required: true }}
+          render={(): JSX.Element => {
+            return (
+              <>
+                <label htmlFor="8" className={styles.accept}>
+                  Accept T&C:
+                  <input id="8" type="checkbox" className={styles.input} />
+                  {errorMessage && <span className={styles.errorMessage}>{errorMessage}</span>}
+                </label>
+              </>
+            );
+          }}
+        />
+      </form>
     </FormWrapper>
   );
 }
